4275a045c2a5ecc850bbcae57d757476bfbae91e
Stephanie Flett <24stephanieflett@usd266.com>
2023-12-01 09:57:56 -0600

    done

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -40,3 +40,67 @@ def average_act_score(scores):
     return total/validScores
 print("average_act_score")
 print("5,36,22,29,25,33,0,50,100 ->",average_act_score([5,36,22,29,25,33,0,50,100]))
+
+####
+
+def all_true(items):
+    count=0
+    for item in items:
+        if item==True:
+            count=count+1
+    if count==len(items):
+        return True
+    else:
+        return False
+print("all_true")
+print("[True, False] ->", all_true([True,False]))
+print("[True,True,True] ->", all_true([True,True,True]))
+
+####
+
+def any_true(items):
+    count=0
+    for item in items:
+        if item==True:
+            count=count+1
+    if count>0:
+        return True
+    else:
+        return False
+print("any_true")
+print("[True, False] ->", any_true([True,False]))
+print("[True,True,True] ->", any_true([True,True,True]))
+print("[False,False] ->",any_true([False,False]))
+
+####
+
+def mostly_true(items):
+    trueCount=0
+    falseCount=0
+    for item in items:
+        if item==True:
+            trueCount=trueCount+1
+        else:
+            falseCount=falseCount+1
+    if trueCount>falseCount:
+            return True
+    else:
+            return False
+print("mostly_true")
+print("[True,False,False] ->", mostly_true([True,False,False]))
+print("[True,True,True] ->", mostly_true([True,True,True]))
+
+####
+
+def has_vowel(characters):
+    vowelCount=0
+    for character in characters:
+        if character in ["a","e","i","o","u"]:
+            vowelCount=vowelCount+1
+    if vowelCount>0:
+        return True
+    else:
+        return False
+print("has_vowel")
+print("[a,g,h,e,v,n] ->",has_vowel(["a","g","h","e","v","n"]))
+print("[b,c,d,f,g] ->",has_vowel(["b","c","d","f","g"]))
