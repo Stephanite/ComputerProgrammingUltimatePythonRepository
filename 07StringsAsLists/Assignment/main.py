606426489253c06db8ee4b0a2ec30e913468cd07
Stephanie Flett <24stephanieflett@usd266.com>
2023-12-07 09:33:03 -0600

    update

--- 07StringsAsLists/Assignment/main.py
+++ 07StringsAsLists/Assignment/main.py
@@ -12,7 +12,7 @@ print("computer and keyboard ->", is_alliteration("computer","keyboard"))
 print("apple and ant ->", is_alliteration("apple","ant"))
 
 
-####
+print("")
 
 def count_vowels(word):
     vowelCount=0
@@ -24,7 +24,7 @@ print("count_vowels")
 print("potato ->", count_vowels("potato"))
 print("try ->", count_vowels("try"))
 
-####
+print("")
 
 def count_numbers(string):
     numberCount=0
@@ -37,7 +37,7 @@ print("h311o ->", count_numbers("h311o"))
 print("penguin ->", count_numbers("penguin"))
 print("123YO09 ->", count_numbers("123YO09"))
 
-####
+print("")
 
 def count_target_letters(word,character):
     count=0
@@ -50,7 +50,7 @@ print("count_target_letters")
 print("apple, a ->", count_target_letters("apple", "a"))
 print("smile, h ->", count_target_letters("smile","h"))
 
-####
+print("")
 
 def in_alphabetical_order(word):
     alphabet="a"
@@ -64,7 +64,7 @@ print("in_alphabetical_order")
 print("abcdefg ->", in_alphabetical_order("abcdefg"))
 print("gjfjnvfdn ->", in_alphabetical_order("gjfjnvfdn"))
 
-####
+print("")
 
 def alternate_case(word):
     result=""
@@ -81,7 +81,7 @@ print("alternate_case")
 print("computer ->", alternate_case("computer"))
 print("pizza ->", alternate_case("pizza"))
 
-####
+print("")
 
 def remove_vowels(string):
     result=""
@@ -96,20 +96,21 @@ print("teeth ->", remove_vowels("teeth"))
 print("bananna ->", remove_vowels("bananna"))
 print("sky ->", remove_vowels("sky"))
 
-####
+print("")
 
 def to_camel_case(string):
     result=""
-    for letter in string.title():
+    for letter in string:
         if letter==" ":
             pass
         else:
+            letter=letter.upper()
             result=result+letter
     return result
 print("to_camel_case")
 print("final test today ->", to_camel_case("final test today"))
 
-####
+print("")
 
 def to_snake_case(string):
     result=""
@@ -122,18 +123,30 @@ def to_snake_case(string):
 print("to_snake_case")
 print("final test today ->", to_snake_case("final test today"))
 
-####
+print("")
 
 def without_duplicates(numbers):
     result=[]
-    previousNumber=numbers[0]
+    previousNumber=numbers[0]-1
     for number in numbers:
         if number==previousNumber:
             previousNumber=number
-            pass
         else:
+            previousNumber=number
             result.append(number)
     return result
 print("without_duplicates")
 print("[1,2,5,5,7] ->", without_duplicates([1,2,5,5,7]))
 
+print("")
+
+def filter_valid_act_scores(numbers):
+    validScore=[]
+    for number in numbers:
+        if 1<=number<=36:
+            validScore.append(number)
+        else:
+            pass
+    return validScore
+print("filter_vaild_act_scores")
+print("[1,21,90,47,36,30,24,0] ->", filter_valid_act_scores([1,21,90,47,36,30,24,0]))
