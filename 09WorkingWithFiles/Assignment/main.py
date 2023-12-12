73e27f125136e35398fbd6a92a83d4a76996aaa2
Stephanie Flett <24stephanieflett@usd266.com>
2023-12-12 09:37:01 -0600

    update

--- 09WorkingWithFiles/Assignment/main.py
+++ 09WorkingWithFiles/Assignment/main.py
@@ -1,1 +1,20 @@
 import csv
+
+f=open("../data/4000-most-common-english-words.csv", "r")
+words=f.read().split("\n")
+f.close()
+
+def word_with_most_vowels(wordlist):
+    mostSoFar=0
+    vowelCount=0
+    vowels=["a","e","i","o","u"]
+    mostVowelsWord=""
+    for word in wordlist:
+        if vowels in word:
+            vowelCount=len(vowels)
+        else:
+            pass
+        if vowelCount>mostSoFar:
+            mostVowelsWord=word
+    return mostVowelsWord
+   # print("word with most vowels: ", mostVowelsWord)
